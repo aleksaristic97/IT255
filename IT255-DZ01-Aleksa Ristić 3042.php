e2b4a2f63c9ddcf9091e5f56106359541428c576
unknown <rista300@gmail.com>
2018-02-25 18:04:45 +0100

    IT255-DZ01 Aleksa Ristic 3042

---
+++ IT255-DZ01-Aleksa Ristić 3042.php
@@ -0,0 +1,21 @@
+
+<html>
+
+
+<?php
+
+
+echo "Aleksa Ristić 3042";
+
+echo "<a href = 'https://www.youtube.com/results?search_query=indexi+zivot+je+jedan+ona+bacila'> <br> Link za pesmu</a>";
+
+
+ 
+?>
+
+
+
+
+
+
+</html>
